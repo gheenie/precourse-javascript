ca0d7c4608f1aaea8ce2880e9f41ef32a12de926
infoomics <sbx_94@hotmail.com>
2022-11-25 16:28:07 +0000

    Completed challenge 1.

--- challenges/katas/1-maths.js
+++ challenges/katas/1-maths.js
@@ -3,14 +3,20 @@
 
 function modulo(a, b) {
   // return the remainder of the division a / b
+
+  return a % b;
 }
 
 function squareRoot(n) {
   // return the square root of n
+
+  return Math.sqrt(n);
 }
 
 function raiseToPower(m, n) {
   // return the result of raising m to the nth power
+
+  return Math.pow(m, n);
 }
 
 function formatMoney(amount) {
@@ -23,20 +29,44 @@ function formatMoney(amount) {
    * Amounts should always have 2 decimal numbers even if they are zeroes
    * formatMoney(1) => "£1.00"
    */
+
+  return '£' + amount.toFixed(2);
 }
 
 function calculateCircleArea(r) {
   // return the area of a circle with radius r
   // round result to 3 decimal numbers
+
+  let areaOfCircle = Math.PI * r * r;
+  areaOfCircle = parseFloat( areaOfCircle.toFixed(3) );
+
+  return areaOfCircle;
 }
 
 function calculateFullTurns(degrees) {
   // return the number of full turns you can make with the provided degrees
   // 1 full turn === 360 degrees
+
+  return Math.floor(degrees / 360);
 }
 
 function findSmallestAndBiggest(arr) {
   // given an array, return a new array containing the smallest and biggest values of the original array.
+
+  let smallAndBig = [arr[0], arr[0]];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < smallAndBig[0]) {
+      smallAndBig[0] = arr[i];
+      continue;
+    }
+
+    if (arr[i] > smallAndBig[1]) {
+      smallAndBig[1] = arr[i];
+    }
+  }
+
+  return smallAndBig;
 }
 
 function splitTheBeans(totalCost, numOfPeople) {
@@ -51,6 +81,8 @@ function splitTheBeans(totalCost, numOfPeople) {
   Note: The Northcoders staff only have whole pound coins. So the cost per person needs to be to the nearest pound coin (we'll give it as a tip!).
 
   */
+
+  return Math.ceil(totalCost / numOfPeople);
 }
 
 // ---------- Do not change the code below this line --------------
