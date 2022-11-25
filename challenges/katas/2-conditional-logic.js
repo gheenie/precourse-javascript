6145cf2abb9ad0ce203e8909d5a6fc77be064782
infoomics <sbx_94@hotmail.com>
2022-11-25 16:47:05 +0000

    Completed challenge 2.

--- challenges/katas/2-conditional-logic.js
+++ challenges/katas/2-conditional-logic.js
@@ -3,22 +3,54 @@
 
 function areValuesEqual(left, right) {
   // return true if the passed arguments are strictly equal
+
+  if (left === right) {
+    return true;
+  }
+
+  return false;
 }
 
 function areValuesNotEqual(left, right) {
   // return true if the passed arguments are NOT equal
+
+  if (left != right) {
+    return true;
+  }
+
+  return false;
 }
 
 function isFromThe60s(year) {
   // return true if the year is in the 1960's
+
+  const yearFirstThreeDigits = year.toString().substr(0, 3);
+
+  if (yearFirstThreeDigits === '196') {
+    return true;
+  }
+
+  return false;
 }
 
 function isEvenLength(string) {
   // return true if string has an even length
+
+  if (string.length % 2 === 0) {
+    return true;
+  }
+
+  return false;
 }
 
 function areStringsEqualCaseInsensitive(left, right) {
   // return true if the passed strings are equal ignoring case
+
+  if (left.toLowerCase() === right.toLowerCase()) {
+    return true;
+  }
+
+  return false;
 }
 
 // ---------- Do not change the code below this line --------------
