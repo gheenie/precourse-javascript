97373be5bc884d5d3ab824e21cd4a11e535d80c4
infoomics <sbx_94@hotmail.com>
2022-12-05 19:34:11 +0000

    Completed advanced challenges.

--- advanced-challenges/advanced-challenges.js
+++ advanced-challenges/advanced-challenges.js
@@ -2,6 +2,8 @@ function getEvenNumbers(nums) {
   /*
     This function takes an array of integers and returns an array containing only even integers
   */
+
+  return nums.filter((x) => x % 2 === 0);
 }
 
 function flipBooleans(bools) {
@@ -9,6 +11,12 @@ function flipBooleans(bools) {
     This function takes an array of booleans and should return an array of the opposite booleans.
     E.g. [true, true, false] => [false, false, true]
   */
+
+  return bools.map((x) => {
+    if (x === true) return false;
+
+    return true;
+  });
 }
 
 function translateKey(student, keyToChange, translation) {
@@ -33,12 +41,25 @@ function translateKey(student, keyToChange, translation) {
       job: 'Artist'
     }
   */
+  
+  let studentCopy = JSON.parse( JSON.stringify(student) );
+
+  Object.defineProperty( studentCopy, translation, Object.getOwnPropertyDescriptor(studentCopy, keyToChange) );
+  delete studentCopy[keyToChange];
+
+  return studentCopy;
 }
 
 function findFirstDentist(people) {
   /*
     This function takes an array of people objects and returns the first found dentist object from the array.
   */
+  
+  for (let i = 0; i < people.length; i++) {
+    if (people[i]['isDentist']) return people[i];
+  }
+
+  return null;
 }
 
 function tallyPeopleInManchester(people) {
@@ -49,6 +70,14 @@ function tallyPeopleInManchester(people) {
 
     The function should return the number of people who live in the city of Manchester
   */
+
+  /*return people.reduce(function(count, person) {
+    if (person.lives.city === 'Manchester') return count + 1;
+    
+    return count;
+  }, 0);*/
+
+  return people.filter((person) => person.lives.city === 'Manchester').length;
 }
 
 function getPugOwners(dogs) {
@@ -61,6 +90,8 @@ function getPugOwners(dogs) {
     ]
     will return ['Izzi', 'Anat']
   */
+
+  return dogs.filter((dog) => dog.breed === 'Pug').map((dog) => dog.owner);
 }
 
 function pluraliseKeys(obj) {
@@ -90,6 +121,18 @@ function pluraliseKeys(obj) {
       ]
     }
   */
+
+  const objCopy = JSON.parse( JSON.stringify(obj) );
+
+  const properties = Object.getOwnPropertyNames(objCopy);
+  const propsToChange = properties.filter( (prop) => Array.isArray(objCopy[prop]) );
+  
+  propsToChange.map((propToChange) => {
+    Object.defineProperty( objCopy, propToChange + 's', Object.getOwnPropertyDescriptor(objCopy, propToChange) );
+    delete objCopy[propToChange];
+  });
+
+  return objCopy;
 }
 
 function getWordLengths(str) {
@@ -97,6 +140,10 @@ function getWordLengths(str) {
     This function takes a string and returns an array of the lengths of each word in the string.
     E.g. 'pineapple and black bean curry' => [9, 3, 5, 4, 5]
   */
+
+  if (str === '') return [];
+
+  return str.split(' ').map((word) => word.length);
 }
 
 function getPalindromes(words) {
@@ -105,6 +152,16 @@ function getPalindromes(words) {
     A palindrome is a word that is spelled the same way backwards.
     E.g. ['foo', 'racecar', 'pineapple', 'porcupine', 'tacocat'] =>  ['racecar', 'tacocat']
   */
+
+  return words.filter((word) => {
+    let reversedWord = '';
+
+    for (let i = word.length - 1; i >= 0; i--) {
+      reversedWord += word.charAt(i);
+    }
+
+    if (word === reversedWord) return true;
+  });
 }
 
 function replaceLettersWithXs(str) {
@@ -112,6 +169,8 @@ function replaceLettersWithXs(str) {
     This function will receive a string with a mix of characters. It should return the string with all letters replaced by dashes ('X').
     For example 'I love Greg' should return 'X XXXX XXXX', and 'Hard as 1, 2, 3' should return 'XXXX XX 1, 2, 3'.
   */
+
+  return str.replace(/[A-Za-z]/g, 'X');
 }
 
 function validMobileNumber(num) {
@@ -122,6 +181,10 @@ function validMobileNumber(num) {
     It may also begin with '00447' followed by 9 more digits.
     Anything else is invalid.
   */
+
+  if ( num.match(/^(07|\+447|00447)\d{9}$/) ) return true;
+
+  return false;
 }
 
 function sumDigitsFromString(str) {
@@ -131,6 +194,11 @@ function sumDigitsFromString(str) {
     'foo99cat' => 18
     Tip: For this one, it might be useful to know that the value `NaN` in JavaScript behaves oddly. For example, if you do `typeof NaN` it tells you it's a `"number"`. Odd, eh?
   */
+  
+  // Give non-digits a value of 0 and simply add them all later.
+  const zeroedLetters = str.replace(/[^\d]/g, 0);
+
+  return zeroedLetters.split('').reduce((count, char) => count + parseInt(char), 0);
 }
 
 function getWilliams(arr) {
@@ -138,6 +206,8 @@ function getWilliams(arr) {
     This function takes an array of full names and returns an array containing only the people whose last name is Williams.
     E.g. ['Charlotte Jones', 'Rebecca Williams', 'Harry Williams', 'John Williamson', 'William Jones'] => ['Rebecca Williams', 'Harry Williams']
   */
+
+  return arr.filter( (name) => name.match(/Williams$/) );
 }
 
 
@@ -149,6 +219,16 @@ function getFactorials(nums) {
     The factorial of a number is the product of that number and all the integers below it.
     E.g. the factorial of 4 is 4 * 3 * 2 * 1 = 24
   */
+
+  return nums.map((num) => {
+    return factorialise(num);
+
+    function factorialise(x) {
+      if (x === 1) return 1;
+      
+      return x * factorialise(x - 1);
+    };
+  });
 }
 
 function largestNumber(num) {
@@ -158,6 +238,33 @@ function largestNumber(num) {
     E.g. if num is 9, the function should return 9.
     E.g. if num is 581 the function should return 851.
   */
+
+  // To represent how many times a digit appears in the number.
+  const digitCounts = {
+    '0': 0,
+    '1': 0,
+    '2': 0,
+    '3': 0,
+    '4': 0,
+    '5': 0,
+    '6': 0,
+    '7': 0,
+    '8': 0,
+    '9': 0,
+  };
+
+  const digits = String(num).split('');
+  digits.map((digit) => digitCounts[digit] += 1);
+
+  let largestNum = '';
+
+  for (let i = 9; i >= 0; i--) {
+    const iStr = String(i);
+    const digitCount = digitCounts[iStr];
+    largestNum += iStr.repeat(digitCount);
+  }
+
+  return parseInt(largestNum);
 }
 
 function generateMatrix(n) {
@@ -170,6 +277,8 @@ function generateMatrix(n) {
       [null, null, null]
     ]
   */
+
+  return Array(n).fill( Array(n).fill(null) );
 }
 
 function findWrongWayFruit(orchard) {
@@ -181,6 +290,15 @@ function findWrongWayFruit(orchard) {
     Your function should return its index position. E.g. in this example, 4.
     NB The fruit will not always be apple but it will always be an orchard of the same kind of fruit.
   */
+
+  const firstFruit = orchard[0];
+
+  // If the wrong fruit is at index 0
+  if (orchard[1] !== firstFruit && orchard[2] !== firstFruit) return 0;
+
+  for (let i = 1; i < orchard.length; i++) {
+    if (orchard[i] !== firstFruit) return i;
+  }
 }
 
 function pairDNA(dna) {
@@ -190,6 +308,21 @@ function pairDNA(dna) {
     If any nonsensical letters are passed as DNA they should be ignored.
     E.g. 'GYTC' => ['GC', 'TA', 'CG']
   */
+
+  const pairs = {
+    'G': 'C',
+    'C': 'G',
+    'A': 'T',
+    'T': 'A'
+  };
+
+  // Clean the string then remove invalid bases.
+  const bases = dna
+    .toUpperCase()
+    .split('')
+    .filter((base) => pairs.hasOwnProperty(base));
+
+  return bases.map((base) => base + pairs[base]);
 }
 
 function tallyHashtagsAndMentions(str) {
@@ -201,6 +334,11 @@ function tallyHashtagsAndMentions(str) {
     The function should return an object describing the number of hashtags and mentions found:
     { hashtags: 2, mentions: 1 }
   */
+
+  const hashtags = (str.match(/#/g) || []).length;
+  const mentions = (str.match(/@/g) || []).length;
+
+  return { hashtags: hashtags, mentions: mentions };
 }
 
 // ---------- Do not change the code below this line --------------
